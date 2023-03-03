3a2cacb791953a39c4cff6097a290c088b2b66c6
NCStorey <ncsansom@hotmail.co.uk>
2023-03-03 21:35:22 +0000

    initial test to make sure inquirer is working

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter the Team Managers name',
+        name: 'teamManagers-name',
+        validate: (input) => {
+            if (input == ''){
+            return 'Oops looks like you didnt enter a title. Please enter your project title'
+            }
+            else {return true}}
+    }
+]
+
+inquirer
+.prompt(questions)
+.then(answers => {
+    console.log(answers)
+})
+.catch((error) => {
+    if (error.isTtyError) {
+        console.log('Could not be rendered in the current environment')
+    } else {
+        console.log('Something else went wrong')
+    }
+})
