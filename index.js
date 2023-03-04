8f363a27bf435368d36f3a001428b2dc07f61106
NCStorey <ncsansom@hotmail.co.uk>
2023-03-04 20:49:44 +0000

    working through inquierer logic index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const questions = [
+const questionsManager = [
     {
         type: 'input',
         message: 'Please enter the Team Managers name',
@@ -24,13 +24,49 @@ const questions = [
             return 'Oops looks like you didnt enter a title. Please enter your project title'
             }
             else {return true}}
+    },
+
+
+]
+
+const buildOptions = [
+{
+    type: 'list',
+    message: 'Please select one of the following options',
+    choices: ['Add an Engineer', 'Add an intern', 'Finish building the team'],
+    default: 'Finish building the team',
+    name: 'buildOptions',
+}]
+
+
+const questionsEngineer = [
+
+    {
+        type: 'input',
+        message: 'Please enter the Engineers name',
+        name: 'engineer-name',
+        default: 'Engineer',
     }
+
+]
+
+const questionsIntern = [
+
+    {
+        type: 'input',
+        message: 'Please enter the Interns name',
+        name: 'intern-name',
+        default: 'Intern',
+    }
+
 ]
 
 inquirer
-.prompt(questions)
+.prompt(questionsManager)
 .then(answers => {
     console.log(answers)
+
+
 })
 .catch((error) => {
     if (error.isTtyError) {
@@ -39,3 +75,37 @@ inquirer
         console.log('Something else went wrong')
     }
 })
+
+
+// let buildOption = answers.buildOptions
+
+// switch (buildOption){
+
+//     case 'Add an Engineer':
+        
+//     inquirer.prompt(questionsEngineer)
+//     .then(answers => {
+//         console.log(answers)
+//     })
+
+//     break;
+
+//     case 'Add an intern':
+        
+//     inquirer.prompt(questionsIntern)
+//     .then(answers => {
+//         console.log(answers)
+//     })
+
+//     break;
+
+//     case 'Finish building the team':
+        
+//     console.log('you choose finish building the team')
+    
+//     break;
+
+//     default:
+//         console.log('Not sure how this happened?')
+
+// }
